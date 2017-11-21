d95101a70af466c481d2c163868de97a63465c2b
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-21 14:44:10 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(number)
   end
    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  number = 5
+  while number > 0
+    puts "#{number}"
+    number -= 1
+  end
+ end
+end 
